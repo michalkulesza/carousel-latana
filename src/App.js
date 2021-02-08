4e044b25ff389fc1d720ee2e65bf6ac58f31ac20
Michal <kulesza.michal@gmail.com>
2021-02-08 22:08:41 +0100

    Render carousel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
+import useWindowSize from "./hooks/useWindowSize";
 
-import { Button } from "./components";
+import { Carousel } from "./components";
+import { ITEMS } from "./fixtures/data";
 
 const App = () => {
+	const { width } = useWindowSize();
+	const carouselItemsOnScreen = width < 500 ? 1 : width < 900 ? 2 : 3;
+
 	return (
 		<div className="App">
-			<h1>hello world</h1>
-			<Button type="arrow" />
-			<Button type="arrow" direction="alternate" />
-			<Button>Learn more</Button>
+			<Carousel
+				heading="Latana Knowledge Center"
+				subHeading="Everything you need to know about the best brand tracking solution in the world."
+				items={ITEMS}
+				itemsNum={carouselItemsOnScreen}
+			/>
 		</div>
 	);
 };
