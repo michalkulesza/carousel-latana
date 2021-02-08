79203cd4bd9e4309873e22c6e30c74643141e0ec
Michal <kulesza.michal@gmail.com>
2021-02-08 13:44:36 +0100

    + data placeholder + illustrations

---
+++ src/fixtures/data.js
@@ -0,0 +1,52 @@
+import Img1 from "../assets/illustrations/1.svg";
+import Img2 from "../assets/illustrations/2.svg";
+import Img3 from "../assets/illustrations/3.svg";
+import Img4 from "../assets/illustrations/4.svg";
+
+export const ITEMS = [
+	{
+		id: 1,
+		imageSrc: Img1,
+		imageAlt: "Description of an image",
+		heading: "What is Latana?",
+		content: "How does Latana work?\nWhy choose Latana?\nWhat does Latana cost?",
+		buttonContent: "Learn More",
+		buttonSrc: "/",
+	},
+	{
+		id: 2,
+		imageSrc: Img2,
+		imageAlt: "Description of an image",
+		heading: "What is Latana?",
+		content: "How does Latana work?\nWhy choose Latana?\nWhat does Latana cost?",
+		buttonContent: "Learn More",
+		buttonSrc: "/",
+	},
+	{
+		id: 3,
+		imageSrc: Img3,
+		imageAlt: "Description of an image",
+		heading: "What is Latana?",
+		content: "How does Latana work?\nWhy choose Latana?\nWhat does Latana cost?",
+		buttonContent: "Learn More",
+		buttonSrc: "/",
+	},
+	{
+		id: 4,
+		imageSrc: Img4,
+		imageAlt: "Description of an image",
+		heading: "What is Latana?",
+		content: "How does Latana work?\nWhy choose Latana?\nWhat does Latana cost?",
+		buttonContent: "Learn More",
+		buttonSrc: "/",
+	},
+	{
+		id: 5,
+		imageSrc: Img2,
+		imageAlt: "Description of an image",
+		heading: "What is Latana?",
+		content: "How does Latana work?\nWhy choose Latana?\nWhat does Latana cost?",
+		buttonContent: "Learn More",
+		buttonSrc: "/",
+	},
+];
